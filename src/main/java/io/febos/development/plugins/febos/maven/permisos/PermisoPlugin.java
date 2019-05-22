89ea5fc7e249e917b7ef5683d82846ab48f901c3
Claudio Miranda <cronosunder@gmail.com>
2019-05-22 18:04:02 -0400

    Fix headers Options

--- src/main/java/io/febos/development/plugins/febos/maven/permisos/PermisoPlugin.java
+++ src/main/java/io/febos/development/plugins/febos/maven/permisos/PermisoPlugin.java
@@ -1,5 +1,6 @@
 package io.febos.development.plugins.febos.maven.permisos;
 
+import com.google.gson.Gson;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.*;
@@ -11,16 +12,22 @@ import org.reflections.util.ConfigurationBuilder;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
-@Mojo(name = "permisos", defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES,
-        requiresDependencyResolution = ResolutionScope.TEST)
+import java.sql.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+/**
+ * blah blah blah
+ *
+ * @goal validate-security
+ * @phase package
+ * @requiresDependencyResolution compile+runtime
+ */
+@Mojo(name = "permisos", requiresProject = true,requiresDirectInvocation = true, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class PermisoPlugin extends AbstractMojo {
     /**
      * Injected by maven to give us a reference to the project so we can get the
@@ -37,6 +44,11 @@ public class PermisoPlugin extends AbstractMojo {
 
     @Parameter(defaultValue = "${project.compileClasspathElements}", readonly = true, required = true)
     public List<String> projectClasspathElements;
+    @Parameter(defaultValue = "cl", readonly = true, required = true)
+    public String pais;
+
+    @Parameter(defaultValue = "desarrollo,pruebas,certificacion,produccion", readonly = true, required = true)
+    public String ambientes;
 
     /**
      * Scans the source code for this module to look for instances of the
@@ -45,11 +57,23 @@ public class PermisoPlugin extends AbstractMojo {
      * @throws MojoExecutionException thrown if there are errors during analysis
      */
     public void execute() throws MojoExecutionException {
+        try {
+            getLog().info("CONFIGURAMOS ");
+            Gson g = new Gson();
+            getLog().info("pais  " + pais);
+            getLog().info("ambientes  " + ambientes);
+            getLog().info("projectClasspathElements  " + projectClasspathElements);
+            getLog().info("annotations " + g.toJson(annotations));
+//            getLog().info("project " + g.toJson(project));
+        } catch (Exception e) {
 
+        }
         // There's nothing to do if there are no annotations configured.
         if (annotations == null || annotations.isEmpty()) {
-            getLog().warn("There are no annotations configured so there's nothing for this plugin to do.");
-            return;
+            annotations=new ArrayList<>();
+            PermisoAnnotation permiso= new PermisoAnnotation();
+            permiso.setName("io.febos.core.validador.Permiso");
+            annotations.add(permiso);
         }
 
         int processedCount = 0;
@@ -100,14 +124,18 @@ public class PermisoPlugin extends AbstractMojo {
     }
 
     private int processAnnotations(URLClassLoader projectClassloader, Reflections reflections) throws Exception {
-        int processedCount = 0;
-        for (PermisoAnnotation sa : annotations) {
+        AtomicInteger processedCount = new AtomicInteger();
+        annotations.parallelStream().forEach(sa -> {
             initAnnotationType(projectClassloader, sa);
             if (sa.getClazz() != null) {
-                processedCount += validateAllAnnotationExpressions(reflections, sa);
+                try {
+                    processedCount.addAndGet(validateAllAnnotationExpressions(reflections, sa));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        return processedCount;
+        });
+        return processedCount.get();
     }
 
     private void initAnnotationType(URLClassLoader projectClassloader, PermisoAnnotation sa) {
@@ -145,14 +173,85 @@ public class PermisoPlugin extends AbstractMojo {
         try {
             Set<Class<?>> casses = reflections.getTypesAnnotatedWith(annoType, true);
             getLog().debug(String.format("Validating expression class : %s", casses));
-            for (Class<?> c : casses) {
-                getLog().debug(String.format("anotated  : %s", c));
-            }
+            casses.parallelStream().forEach(c -> {
+                getLog().debug(String.format("anotated " + pais + " : %s", c));
+                procesarPermiso(pais, c, annoType);
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return processedCount;
+    }
+
+    private void procesarPermiso(String pais, Class<?> clase, Class<? extends Annotation> tipoAnotacion) {
+        getLog().debug(String.format("Validando permisos en  " + pais + " : %s  para %s", clase, tipoAnotacion));
+        Annotation anotacion = clase.getAnnotation(tipoAnotacion);
+        Method[] metods = tipoAnotacion.getDeclaredMethods();
+        HashMap<String, String> valores = new HashMap<>();
+        for (int i = 0; i < metods.length; i++) {
+            try {
+                getLog().debug(String.format("valor de %s  :  %s", metods[i].getName(), metods[i].invoke(anotacion)));
+                valores.put(metods[i].getName().trim(), String.valueOf(metods[i].invoke(anotacion)));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        valores.put("pais", pais);
+        String requestPermisos = new Gson().toJson(valores);
+        getLog().info("REQUEST PERMISOS   " + requestPermisos);
+        asignarPermisos(ambientes, valores);
+    }
+
+    public void asignarPermisos(String ambientes, HashMap<String, String> req) {
+        String server = "";
+        String[] paises = req.get("pais").split(",");
+        for (int i = 0; i < paises.length; i++) {
+            String pais = paises[i];
+            switch (pais) {
+                case "co": {
+                    server = "colombia-cluster.cluster-c3m0bwpzpiz8.us-east-1.rds.amazonaws.com";
+                    break;
+                }
+                case "cl": {
+                    server = "febos-io-chile.cluster-c3m0bwpzpiz8.us-east-1.rds.amazonaws.com";
+                    break;
+                }
+                case "legacy": {
+                    server = "febosb.c3m0bwpzpiz8.us-east-1.rds.amazonaws.com";
+                    break;
+                }
+                default: {
+                    throw new RuntimeException("Pais no permitido o soportado");
+                }
+            }
+
+            String url = "jdbc:mysql://" + server + ":3306";
+            try (Connection conn = DriverManager.getConnection(url, "superadmin", "ia$olution$**")) {
+                conn.setAutoCommit(true);
+                String[] ambientesAr = ambientes.split(",");
+                for (String ambiente : ambientesAr) {
+                    String insert = "INSERT IGNORE INTO febos_" + ambiente + ".permisos\n" +
+                            "(codigo, accion, nombre, descripcion, nivel, actualizado)\n" +
+                            "VALUES(?, ?, ?, ?, ?, ?);";
+                    try (Statement st = conn.prepareStatement(insert)) {
+                        ((PreparedStatement) st).setString(1, req.get("codigo"));
+                        ((PreparedStatement) st).setString(2, req.get("accion"));
+                        ((PreparedStatement) st).setString(3, req.get("nombre"));
+                        ((PreparedStatement) st).setString(4, req.get("descripcion"));
+                        ((PreparedStatement) st).setString(5, req.get("nivel"));
+                        ((PreparedStatement) st).setDate(6, new java.sql.Date(new java.util.Date().getTime()));
+                        getLog().info("PERMISO " + st.toString());
+                        ((PreparedStatement) st).executeUpdate();
+                    }
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
 
 }
